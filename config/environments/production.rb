2145ca76e7dcb6e3ba9fb606b59aca8db62b6bac
Corina Todoran <corina.todoran@takeofflabs.com>
2020-04-09 16:21:58 +0300

    a

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,9 +90,9 @@ Rails.application.configure do
   end
 
   # Do not dump schema after migrations.
-  config.action_mailer.default_url_options = { :host => 'conflict-scenarios.herokuapp.com' }
-  config.active_record.dump_schema_after_migration = false
-  config.action_mailer.perform_deliveries = true
+  # config.action_mailer.default_url_options = { :host => 'conflict-scenarios.herokuapp.com' }
+  # config.active_record.dump_schema_after_migration = false
+  # config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       :address              => "smtp.gmail.com",
