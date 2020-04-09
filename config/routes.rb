6ed65d654782850d2ff57d79dff7aae6b26b3428
Corina Todoran <corina.todoran@takeofflabs.com>
2020-04-09 16:46:53 +0300

    a

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'visitors#index'
-  devise_for :users, controllers: { omniauth_callbacks: "omniauth_callbacks", confirmations: 'confirmations' }
+  devise_for :users, controllers: { omniauth_callbacks: "omniauth_callbacks", confirmations: 'users/confirmations' }
   resources :users do
     member do
       get :confirm_email
