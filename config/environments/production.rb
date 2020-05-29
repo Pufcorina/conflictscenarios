87bad4fefee7c79044d2d73635c2ebd0243439ae
Corina Todoran <todorananacorina13@gmail.com>
2020-05-29 12:47:35 +0300

    change url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'conflict-scenarios.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'fathomless-journey-13376.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       user_name:      'todorananacorina13@gmail.com',
